6a57e922909ece044824eba08074be675f28b39a
Yuriy Zhmurko <777zhm@gmail.com>
2019-01-30 15:03:44 +0200

    before work

--- templates/block-packages.php
+++ templates/block-packages.php
@@ -7,16 +7,19 @@
 		<div class="paсkages_box">
 			<?php if( have_rows('packages') ): ?>
 			<div class="paсkages_list">
-				<?php $package_number = 1; while( have_rows('packages') ): the_row(); ?>
-				<div class="one_paсkage_title" data-package_number='<?php echo $package_number; ?>'><div class="title"><?php the_sub_field('package_heading');?></div></div>
+				<?php $package_number = 1; while( have_rows('packages') ): the_row(); 
+				$class = ($package_number == 1 )?'active':''; ?>
+				<div class="<?php echo $class; ?> one_paсkage_title" data-package_number='<?php echo $package_number; ?>'><div class="title"><?php the_sub_field('package_heading');?></div></div>
 				<?php $package_number++; endwhile; ?>
 			</div>
 			<?php endif; ?>
 
 			<?php if( have_rows('packages') ): ?>
 			<div class="paсkages_content">
-				<?php $package_number = 1; while( have_rows('packages') ): the_row(); ?>
-				<div class="one_package_content <?php echo ('package_content_'.$package_number); ?>">
+				<?php $package_number = 1; while( have_rows('packages') ): the_row();
+
+				$class = ($package_number == 1 )?'active':''; ?>
+				<div class="one_package_content <?php echo $class.' package_content_'.$package_number; ?>">
 
 					<?php if( have_rows('package_elements') ): ?>
 						<?php $package_element_numder = 1; while( have_rows('package_elements') ): the_row(); ?>
